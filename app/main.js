f1bfe54e8d7aeb66337bc2a136fbc43b63acf078
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2025-06-14 14:48:57 +0530

    update

--- app/main.js
+++ app/main.js
@@ -1,29 +1,29 @@
 function matchPattern(inputLine, pattern) {
-  if (pattern.length === 1) {
-    return inputLine.includes(pattern);
-  } else {
-    throw new Error(`Unhandled pattern ${pattern}`);
-  }
+    if (pattern.length === 1) {
+        return inputLine.includes(pattern);
+    } else {
+        throw new Error(`Unhandled pattern ${pattern}`);
+    }
 }
 
 function main() {
-  const pattern = process.argv[3];
-  const inputLine = require("fs").readFileSync(0, "utf-8").trim();
+    const pattern = process.argv[3];
+    const inputLine = require("fs").readFileSync(0, "utf-8").trim();
 
-  if (process.argv[2] !== "-E") {
-    console.log("Expected first argument to be '-E'");
-    process.exit(1);
-  }
+    if (process.argv[2] !== "-E") {
+        console.log("Expected first argument to be '-E'");
+        process.exit(1);
+    }
 
-  // You can use print statements as follows for debugging, they'll be visible when running tests.
-  console.error("Logs from your program will appear here");
+    // You can use print statements as follows for debugging, they'll be visible when running tests.
+    console.error("Logs from your program will appear here");
 
-  // Uncomment this block to pass the first stage
-  // if (matchPattern(inputLine, pattern)) {
-  //   process.exit(0);
-  // } else {
-  //   process.exit(1);
-  // }
+    // Uncomment this block to pass the first stage
+    if (matchPattern(inputLine, pattern)) {
+        process.exit(0);
+    } else {
+        process.exit(1);
+    }
 }
 
 main();
